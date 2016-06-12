fc27eb40e6ac971585a7d3a9f5c9c37426218cbb
Jack Laptop <jackcrawford2008@gmail.com>
2016-06-12 12:55:35 -0500

    Getting Started

---
+++ fun_times_with_jack.php
@@ -0,0 +1,31 @@
+<?php
+//jack's lil' bag o' tricks
+
+
+$jack_test_number = "9185211947@txt.att.net";
+//This does work, but not from MAMP
+var_dump(mail( "jcrawford17@hollandhall.org", '', 'Testing' ));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
